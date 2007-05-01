bloomberg/chromium.bb

commit 3c384a9ad5f964709a237cfe035ea5d6df2da5fa

Add new buffer object type for kernel allocations that don't initially have a user mapping. (cherry picked from commit 2e21779992bd5026d8ec4dea52466377dbe5a0ed)

shared-core/drm.h
@@ -795,6 +795,7 @@ typedef struct drm_fence_arg {
 
 typedef enum {
 	drm_bo_type_dc,
+	drm_bo_type_kernel, /* for initial kernel allocations */
 	drm_bo_type_user,
 	drm_bo_type_fake
 }drm_bo_type_t;

linux-core/drm_bo.c
@@ -89,6 +89,9 @@ static int drm_bo_vm_pre_move(drm_buffer_object_t * bo, int old_is_pci)
 #ifdef DRM_ODD_MM_COMPAT
 	int ret;
 
+	if (!bo->map_list.map)
+		return 0;
+
 	ret = drm_bo_lock_kmm(bo);
 	if (ret)
 		return ret;
@@ -96,6 +99,9 @@ static int drm_bo_vm_pre_move(drm_buffer_object_t * bo, int old_is_pci)
 	if (old_is_pci)
 		drm_bo_finish_unmap(bo);
 #else
+	if (!bo->map_list.map)
+		return 0;
+
 	drm_bo_unmap_virtual(bo);
 #endif
 	return 0;
@@ -105,6 +111,9 @@ static void drm_bo_vm_post_move(drm_buffer_object_t * bo)
 {
 #ifdef DRM_ODD_MM_COMPAT
 	int ret;
+
+	if (!bo->map_list.map)
+		return;
 
 	ret = drm_bo_remap_bound(bo);
 	if (ret) {
@@ -127,6 +136,11 @@ static int drm_bo_add_ttm(drm_buffer_object_t * bo)
 
 	switch (bo->type) {
 	case drm_bo_type_dc:
+		bo->ttm = drm_ttm_init(dev, bo->mem.num_pages << PAGE_SHIFT);
+		if (!bo->ttm)
+			ret = -ENOMEM;
+		break;
+	case drm_bo_type_kernel:
 		bo->ttm = drm_ttm_init(dev, bo->mem.num_pages << PAGE_SHIFT);
 		if (!bo->ttm)
 			ret = -ENOMEM;

CodeLinaro-mirror/le_platform_vendor_opensource_display_libdrm

commit 9bfe62c14edd9b86bfffe49d4ce84e24549112c0

Fix userspace ABI breakage from 3c384a9ad5f964709a237cfe035ea5d6df2da5fa.

shared-core/drm.h
@@ -788,9 +788,9 @@ typedef struct drm_fence_arg {
 
 typedef enum {
 	drm_bo_type_dc,
-	drm_bo_type_kernel, /* for initial kernel allocations */
 	drm_bo_type_user,
-	drm_bo_type_fake
+	drm_bo_type_fake,
+	drm_bo_type_kernel, /* for initial kernel allocations */
 }drm_bo_type_t;
 
 