bloomberg/chromium.bb

commit 87fdd32c87796482638de54988027d0aca0c4f12

Add NetBSD atomic ops support.
Signed-off-by: Thomas Klausner <wiz@NetBSD.org>
Reviewed-by: Maarten Lankhorst <maarten.lankhorst@canonical.com>
Signed-off-by: Maarten Lankhorst <maarten.lankhorst@ubuntu.com>

xf86atomic.h
@@ -75,21 +75,28 @@ typedef struct {
 
 #endif
 
-#if defined(__sun) && !defined(HAS_ATOMIC_OPS)  /* Solaris & OpenSolaris */
+#if (defined(__sun) || defined(__NetBSD__)) && !defined(HAS_ATOMIC_OPS)  /* Solaris & OpenSolaris & NetBSD */
 
 #include <sys/atomic.h>
 #define HAS_ATOMIC_OPS 1
 
-typedef struct { uint_t atomic; } atomic_t;
+#if defined(__NetBSD__)
+#define _ATOMIC_TYPE int
+#else
+#define _ATOMIC_TYPE uint_t
+#endif
+
+typedef struct { _ATOMIC_TYPE atomic; } atomic_t;
 
 # define atomic_read(x) (int) ((x)->atomic)
-# define atomic_set(x, val) ((x)->atomic = (uint_t)(val))
+# define atomic_set(x, val) ((x)->atomic = (_ATOMIC_TYPE)(val))
 # define atomic_inc(x) (atomic_inc_uint (&(x)->atomic))
 # define atomic_dec_and_test(x) (atomic_dec_uint_nv(&(x)->atomic) == 0)
 # define atomic_add(x, v) (atomic_add_int(&(x)->atomic, (v)))
 # define atomic_dec(x, v) (atomic_add_int(&(x)->atomic, -(v)))
 # define atomic_cmpxchg(x, oldv, newv) atomic_cas_uint (&(x)->atomic, oldv, newv)
 
+#undef _ATOMIC_TYPE
 #endif
 
 #if ! HAS_ATOMIC_OPS

CodeLinaro-mirror/le_platform_vendor_opensource_display_libdrm

commit 387b5b4233761e5b134cfd334449c5782292d4ce

Stop undefining _ATOMIC_TYPE in Solaris/NetBSD section of xf86atomic.h
Since the atomic_set() macro references _ATOMIC_TYPE, undefining it
broke that macro, resulting in build errors of:
"intel_bufmgr_gem.c", line 775: undefined symbol: _ATOMIC_TYPE

Now that we're leaving the macro in the namespace, renamed it to start
with LIBDRM_ instead of _ to avoid polluting others namespace.

Regression introduced in 87fdd32c8779648 Add NetBSD atomic ops support.

Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Reviewed-By: Thomas Klausner <wiz@NetBSD.org>

xf86atomic.h
@@ -81,22 +81,21 @@ typedef struct {
 #define HAS_ATOMIC_OPS 1
 
 #if defined(__NetBSD__)
-#define _ATOMIC_TYPE int
+#define LIBDRM_ATOMIC_TYPE int
 #else
-#define _ATOMIC_TYPE uint_t
+#define LIBDRM_ATOMIC_TYPE uint_t
 #endif
 
-typedef struct { _ATOMIC_TYPE atomic; } atomic_t;
+typedef struct { LIBDRM_ATOMIC_TYPE atomic; } atomic_t;
 
 # define atomic_read(x) (int) ((x)->atomic)
-# define atomic_set(x, val) ((x)->atomic = (_ATOMIC_TYPE)(val))
+# define atomic_set(x, val) ((x)->atomic = (LIBDRM_ATOMIC_TYPE)(val))
 # define atomic_inc(x) (atomic_inc_uint (&(x)->atomic))
 # define atomic_dec_and_test(x) (atomic_dec_uint_nv(&(x)->atomic) == 0)
 # define atomic_add(x, v) (atomic_add_int(&(x)->atomic, (v)))
 # define atomic_dec(x, v) (atomic_add_int(&(x)->atomic, -(v)))
 # define atomic_cmpxchg(x, oldv, newv) atomic_cas_uint (&(x)->atomic, oldv, newv)
 
-#undef _ATOMIC_TYPE
 #endif
 
 #if ! HAS_ATOMIC_OPS